f84713466f9c1a29791df367afc7379245855ef5
Sébastien VÉDRINE <sebastien94170@gmail.com>
2019-02-19 19:38:56 +0100

    Vue + dependencies setup

---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'VÉDRINE To do app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  plugins: ['~plugins/vuetify'],
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    vendor: ['axios', 'vuetify'],
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      /*if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }*/
+    }
+  }
+}
+
